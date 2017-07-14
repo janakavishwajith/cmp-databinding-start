948211bae9d14fb38d676724ac2a1129bffbbb2c
janakavishwjith <janakan@codegen.net>
2017-07-14 15:16:56 +0530

    Life Cycle Hooks test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,5 +23,13 @@ export class AppComponent {
       content: bluePrintData.serverContent
     });
   }
+
+  onChangeFirst(){
+    this.serverElements[0].name = "changed!";
+  }
+
+  onDestroyFirst(){
+    this.serverElements.splice( 0, 1);
+  }
   
 }
--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation,
+  OnChanges, 
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +20,47 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {  
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, 
+  AfterContentInit, AfterContentChecked, OnDestroy, AfterViewInit {  
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
+  @ViewChild('heading') header: ElementRef;
+  @ContentChild('contentPara') contentPara: ElementRef;
 
-  constructor() { }
+  constructor() {
+    console.log("Constructor");
+   }
 
   ngOnInit() {
+    console.log("NG on init");
+    console.log("textContent", this.header.nativeElement.textContent);
+    console.log("Content text", this.contentPara.nativeElement.textContent);
+  }
+
+  ngAfterViewInit(){
+    console.log("After View init");
+    console.log("textContent", this.header.nativeElement.textContent);
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+  }
+
+  ngDoCheck(){
+    console.log("NG do check");
+  }
+
+  ngAfterContentInit(){
+    console.log("After Content Init");
+    console.log("Content text", this.contentPara.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log("After Content checked");
+  }
+
+  ngOnDestroy(){
+    console.log("Component destroy");
   }
 
 }
